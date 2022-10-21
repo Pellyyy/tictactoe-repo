3820988cf3241ac124bd6fc81d59e6d36ddf5c98
Pellyyy <zabookinkajou@protonmail.com>
2022-10-21 10:53:58 +0200

    Update

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-const xField = `<svg class ="x" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" 
+const xField = `<svg class ="x svg" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" 
 xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 
 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 
 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 
 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>`
-const oField = `<svg class="o" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" 
+const oField = `<svg class="o svg" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" 
 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 2c5.517 0 9.997 4.48 9.997 9.998 
 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 
 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z" fill-rule="nonzero"/></svg>`
@@ -17,6 +17,8 @@ const cell1 = document.getElementById("1"),
     cell8 = document.getElementById("8"),
     cell9 = document.getElementById("9")
 
+const winMsg = document.querySelector(".win-msg")
+
 let turn = 0, // 0 = X, 1 = O
     round = 0,
     win = false,
@@ -27,14 +29,20 @@ let turn = 0, // 0 = X, 1 = O
     }
 
 function drawField(cell) {
+    if (cell.dataset.value !== "") {
+        return false // guard clause
+    }
+
     if (turn === 0) {
         cell.innerHTML = xField // render svg
         cell.dataset.value = "X" // update html data value
         turn++ // switch player
+        return true
     } else {
         cell.innerHTML = oField
         cell.dataset.value = "O"
         turn--
+        return true
     }
 }
 
@@ -65,7 +73,7 @@ function checkWin() {
         return true
     } else if (
         cell1.dataset.value === cell4.dataset.value && // checks for first vertical win condition
-        cell4.dataset.value === cell4.dataset.value &&
+        cell4.dataset.value === cell7.dataset.value &&
         cell7.dataset.value !== ""
     ) {
         return true
@@ -98,21 +106,56 @@ function checkWin() {
     }
 }
 
+function initializeGame() {
+    win = false
+    tie = false
+    turn = 0
+    round = 0
+    winMsg.textContent = ""
+    cell1.dataset.value = ""
+    cell2.dataset.value = ""
+    cell3.dataset.value = ""
+    cell4.dataset.value = ""
+    cell5.dataset.value = ""
+    cell6.dataset.value = ""
+    cell7.dataset.value = ""
+    cell8.dataset.value = ""
+    cell9.dataset.value = ""
+    document.querySelectorAll(".svg").forEach((svg) => {
+        svg.remove()
+    })
+}
+
 document.querySelectorAll(".cell").forEach((cell) => {
-    cell.addEventListener(
-        "click",
-        () => {
-            drawField(cell)
-            round++
-            if (checkTie()) {
-                tie = true
-                console.log(tie)
+    cell.addEventListener("click", () => {
+        if (!drawField(cell)) {
+            return
+        }
+        round++
+        if (checkWin()) {
+            win = true
+            if (turn === 0) {
+                winMsg.textContent = "O won! Click to restart."
+                winMsg.addEventListener("click", () => {
+                    initializeGame()
+                })
+            } else {
+                winMsg.textContent = "X won! Click to restart."
+                winMsg.addEventListener("click", () => {
+                    initializeGame()
+                })
             }
-            if (checkWin()) {
-                win = true
-                console.log(win)
-            }
-        },
-        { once: true }
-    )
+        }
+        if (checkTie()) {
+            tie = true
+            winMsg.textContent = "Tie!"
+        }
+    })
 })
+
+// TODOS:
+// - disable inputs when games have been won
+// - display score
+// - store score in local storage
+// - button to wipe score
+// - animate on win
